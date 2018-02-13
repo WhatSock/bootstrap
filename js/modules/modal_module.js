3a8d904c8e376bcb9f92dd1fcdc07258b7f06dbe
Bryan Garaventa <bryan.garaventa@whatsock.com>
2018-02-13 10:02:02 -0800

    Updated the Modal Module to include refactoring contributions by Danny Allen.

--- js/modules/modal_module.js
+++ js/modules/modal_module.js
@@ -1,7 +1,7 @@
 /*!
-Modal Module R1.8
-Copyright 2010-2017 Bryan Garaventa (WhatSock.com)
-Copyright 2018 Danny Allen (dannya.com) / Wonderscore Ltd (wonderscore.co.uk)
+Modal Module R1.9
+Copyright 2010-2018 Bryan Garaventa (WhatSock.com)
+Refactoring Contributions Copyright 2018 Danny Allen (dannya.com) / Wonderscore Ltd (wonderscore.co.uk)
 Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under the terms of the Open Source Initiative OSI - MIT License
 */
 
